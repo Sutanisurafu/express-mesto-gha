e98d3d25552850051f2767c82c6bdde5953a4953
Sutanisurafu <sutanisurafu@yandex.ru>
2023-05-22 21:32:10 +0500

    94%

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,4 +1,4 @@
-const Card = require('../models/card');
+const Card = require("../models/card");
 
 module.exports.createCard = (req, res) => {
   // eslint-disable-next-line no-underscore-dangle
@@ -18,30 +18,37 @@ module.exports.getCards = (req, res) => {
 module.exports.deleteCard = (req, res) => {
   // eslint-disable-next-line no-underscore-dangle
   Card.findByIdAndRemove(req.params.cardId)
-  .then((data) => res.send('Удаление прошло успешно!', data))
-  .catch(err => {
-    if (err.name === 'ValidationError') {
-    next(err)
-    } else {
-    next(err)
-    }
+    .then((data) => res.send("Удаление прошло успешно!", data))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        next(err);
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.likeCard = (req, res) =>
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true }
+  )
+    .then((data) => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({ message: "Карточки с таким id несуществует" });
+      }
     })
-    
-}
+    .then((data) => res.send(data))
+    .catch((err) => res.status(400).send({ message: err.message }));
 
-module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-  { new: true },
-)
-.then((data) => res.send(data))
-.catch((err) => res.status(500).send({ message: err }));
-
-module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $pull: { likes: req.user._id } }, // убрать _id из массива
-  { new: true },
-)
-.then((data) => res.send(data))
-.catch((err) => res.status(500).send({ message: err }));
-
+module.exports.dislikeCard = (req, res) =>
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true }
+  )
+    .then((data) => res.send(data))
+    .catch((err) => res.status(500).send({ message: err }));

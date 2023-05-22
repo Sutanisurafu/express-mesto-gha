b36a265075b5f7a1a78fc801d39f0f42cbd71479
Sutanisurafu <sutanisurafu@yandex.ru>
2023-05-22 20:23:54 +0500

    92%

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({ name, about, avatar }))
+    .then((user) => res.send({ data: user }))
     .catch(err => {
         res.status(400).send({ message: err.message})
       })
@@ -18,8 +18,8 @@ module.exports.getUsers = (req, res, next) => {
 };
 
 module.exports.getUserById = (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => res.send({ data: user }))
+  User.findById(req.params.userId)
+    .then((user) => res.send(user))
     .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
 };
 

2fee169b7d9effb91cf892b2a92bfe677c8fc571
Sutanisurafu <sutanisurafu@yandex.ru>
2023-05-22 19:08:48 +0500

    91%

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,10 @@
 const User = require("../models/user");
 
-module.exports.createUser = (req, res, next) => {
+module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.send(user))
     .catch(err => {
         res.status(400).send({ message: err.message})
       })

581f4c344a5687538d8d61cc84640e8b87bbb2f8
Sutanisurafu <sutanisurafu@yandex.ru>
2023-06-09 13:55:51 +0500

    исправляю ошибки автотестов

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -45,3 +45,10 @@ exports.userIdValidate = celebrate({
     userId: Joi.string().hex().length(24),
   }),
 });
+
+exports.userValidate = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+});
